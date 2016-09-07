0e175c27bce98214f596d647cb9fd78c3fbafe5e
Igor Boky <igorboky@gmail.com>
2016-09-07 21:29:04 +0300

    Started to work under controller. Small filter adjustments. Wrap handlebar template generation logic

---
+++ modules/rangeFilter.js
@@ -0,0 +1,54 @@
+;var RangeFilter = (function(){
+    /**
+     * Filter constructor
+     * @constructor
+     */
+    class RangeFilter extends EventEmitter{
+
+        /**
+         * Function that adding new HTML tags into selector
+         * @param selector
+         */
+        init (selector){
+            var div = document.createElement('div');
+            var input = document.createElement('input');
+            var input2 = document.createElement('input');
+
+            /**
+             * Function for append elements
+             * @param to
+             * @param what
+             */
+            function appendThis(to,what) {
+                to.appendChild(what)
+            }
+            var that = this;
+            input.className = 'from';
+            input.addEventListener('change', function(){
+                that.onChange(input.value,input2.value)
+            });
+            input2.className = 'to';
+            input2.addEventListener('change', function(){
+                that.onChange(input.value,input2.value)
+            });
+            appendThis(div,input);
+            appendThis(div,input2);
+            appendThis(document.querySelector(selector), div);
+
+        }
+
+        /**
+         * OnChange function
+         * @param from
+         * @param to
+         */
+        onChange (from,to) {
+            this.emit('change',{
+                from: +from,
+                to: +to
+            })
+        }
+    }
+
+    return RangeFilter
+}());
